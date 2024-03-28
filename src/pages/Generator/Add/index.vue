511253331906b17818085c9b833e9dee50d516d9
xx <937815432@qq,com>
2024-03-28 13:22:24 +0800

    refactor：页面重构 优化写法

--- src/pages/Generator/Add/index.vue
+++ src/pages/Generator/Add/index.vue
@@ -1,14 +1,15 @@
-<script lang="tsx" setup>
+<script lang="ts" setup>
 import { ref } from 'vue'
-import { message } from 'ant-design-vue'
 import BasicInfo from '~/pages/Generator/Add/components/BasicInfo.vue'
 import FileConfig from "~/pages/Generator/Add/components/FileConfig.vue";
 import ModelConfig from "~/pages/Generator/Add/components/ModelConfig.vue";
 import GeneratorFile from "~/pages/Generator/Add/components/GeneratorFile.vue";
+import GeneratorAddRequest = Api.GeneratorAddRequest;
+
+
 
 const current = ref<number>(0)
 function next() {
-  console.log()
   current.value++
 }
 function prev() {
@@ -18,39 +19,48 @@ const steps = [
   {
     title: '基本信息',
     content: '',
+    component: BasicInfo
   },
   {
     title: '文件配置',
     content: 'Second-content',
+    component: FileConfig
   },
   {
     title: '模型配置',
     content: 'Last-content',
+    component: ModelConfig
   },
   {
     title: '生成器文件',
     content: 'Last-content',
+    component: GeneratorFile
   },
 ]
 const items = steps.map(item => ({ key: item.title, title: item.title }))
+
+const formState = ref<GeneratorAddRequest>({
+  name: '',
+  description: '',
+  basePackage: '',
+  version: '',
+  author: '',
+  tags: undefined,
+  picture: '',
+  fileConfig: {},
+  modelConfig: {},
+})
+
+const generateAdd = (formState:GeneratorAddRequest) => {
+  console.log(formState)
+}
 </script>
 
 <template>
   <div>
     <div class="steps-content">
       <a-steps :current="current" :items="items" style="width: 1150px;margin: 0 auto;padding-bottom:30px " />
-      <template v-if="current === 0">
-        <BasicInfo style="margin: 0 auto" />
-      </template>
-      <template v-if="current === 1">
-        <FileConfig/>
-      </template>
-      <template v-if="current === 2">
-        <ModelConfig/>
-      </template>
-      <template v-if="current === 3">
-        <GeneratorFile/>
-      </template>
+      <component :is="steps[current].component" :formState="formState" />
       <div class="steps-action">
         <a-button v-if="current < steps.length - 1" type="primary" @click="next">
           下一步
@@ -58,9 +68,9 @@ const items = steps.map(item => ({ key: item.title, title: item.title }))
         <a-button
           v-if="current === steps.length - 1"
           type="primary"
-          @click="message.success('Processing complete!')"
+          @click="generateAdd(formState)"
         >
-          Done
+          提交
         </a-button>
         <a-button v-if="current > 0" style="margin-left: 8px" @click="prev">
           上一步
